f16dd8d0d605b4702223d6b7e1a445c0282dc718
Derek Dorr <ddorr@quantummetric.com>
2021-10-27 15:49:44 -0400

    Add debug challenge - ChallengeModalFour

--- src/Challenge.tsx
+++ src/Challenge.tsx
@@ -1,25 +1,24 @@
-import { useState } from 'react';
-import './challenge.scss';
-import QuantumMetricLogo from './QuantumMetricLogo.svg';
-import ChallengeModalOne from './ChallengeModalOne';
-import ChallengeModalTwo from './ChallengeModalTwo';
-import ChallengeModalThree from './ChallengeModalThree';
-
+import { useState } from "react";
+import "./challenge.scss";
+import QuantumMetricLogo from "./QuantumMetricLogo.svg";
+import ChallengeModalOne from "./ChallengeModalOne";
+import ChallengeModalTwo from "./ChallengeModalTwo";
+import ChallengeModalThree from "./ChallengeModalThree";
+import ChallengeModalFour from "./ChallengeModalFour";
 
 const modals = [
-ChallengeModalOne,
-ChallengeModalTwo,
-ChallengeModalThree,
-]
-
-
+  ChallengeModalOne,
+  ChallengeModalTwo,
+  ChallengeModalThree,
+  ChallengeModalFour,
+];
 
 const Challenge = () => {
   const [currentModal, setCurrentModal] = useState(null);
 
   const onClose = () => {
     setCurrentModal(null);
-  }
+  };
 
   return (
     <>
---
+++ src/ChallengeModalFour/util.tsx
@@ -0,0 +1,26 @@
+const patterns = {
+  ip: /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/,
+};
+
+/*
+ * A CIDR contains a valid IP address falled by a range indicator
+ * that has an inclusive value between 0 and 32.
+ *
+ * For example: 127.0.0.1/16
+ */
+export const isValidCidr = (cidr: string): boolean => {
+  let [ip, block, ...otherParts] = cidr.split("/");
+
+  // There should only be 2 segments
+  if (otherParts.length) {
+    return false;
+  }
+  const parsedBlock = parseInt(block);
+
+  return (
+    ip &&
+    parsedBlock &&
+    patterns.ip.test(ip) &&
+    (parsedBlock >= 0 || parsedBlock <= 32)
+  );
+};
